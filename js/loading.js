c7dc734b6f5c57cdca5a8accccf5ff89db86cab8
chaeryeongsdog <xian920626@gmail.com>
2025-02-18 08:11:18 +0800

    loading

--- js/loading.js
+++ js/loading.js
@@ -6,21 +6,17 @@ let headPhones = document.getElementById("headPhones");
 sessionStorage.setItem("musicTime", 0);
 
 setTimeout(() => {
-  body.style.backgroundColor = "#71C6D4";
-  loading.style.opacity = 0;
-}, 800);
-setTimeout(() => {
   logo.style.opacity = 1;
-}, 2400);
+}, 1600);
 setTimeout(() => {
   logo.style.opacity = 0;
-}, 4000);
+}, 3200);
 setTimeout(() => {
   headPhones.style.opacity = 1;
-}, 5600);
+}, 4800);
 setTimeout(() => {
   headPhones.style.opacity = 0;
-}, 7200);
+}, 6400);
 setTimeout(() => {
   window.location.href = "homepage.html";
-}, 8600);
+}, 7800);
