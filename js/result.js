5b35aa7fbf2da1c78327c2d7cb754deb7b9cf045
xian <xian920626@gmail.com>
2025-02-24 17:19:10 +0800

    result triangle

--- js/result.js
+++ js/result.js
@@ -2,8 +2,8 @@ let firstBackground = document.getElementById("firstBackground");
 let spaceWordBtn = document.getElementById("spaceWordBtn");
 let spaceDesignBtn = document.getElementById("spaceDesignBtn");
 
-document.getElementById('backgroundMusic').volume = 0.2
-document.getElementById('backgroundMusic').play()
+document.getElementById("backgroundMusic").volume = 0.2;
+document.getElementById("backgroundMusic").play();
 
 setTimeout(() => {
   firstBackground.style.opacity = 0;
@@ -27,31 +27,57 @@ answerText2.innerText = "(" + answer + ")";
 
 let toJPG = document.getElementById("toJPG");
 let card = document.getElementById("card");
-let cardBack = document.getElementById("cardBack")
+let cardBack = document.getElementById("cardBack");
 // let randomCard = Math.floor(Math.random() * 5) + 1;
-let randomCard = 1
+let randomCard = 1;
 
 if (randomCard == 1) {
   toJPG.style.backgroundImage = "url('../img/NEW SPACE CARD-01.png')";
   card.style.backgroundImage = "url('../img/NEW SPACE CARD-01.png')";
-  cardBack.src = "../img/card_back01.png"
+  cardBack.src = "../img/card_back01.png";
 } else if (randomCard == 2) {
   toJPG.style.backgroundImage = "url('../img/NEW SPACE CARD-02.png')";
   card.style.backgroundImage = "url('../img/NEW SPACE CARD-02.png')";
-  cardBack.src = "../img/card_back02.png"
+  cardBack.src = "../img/card_back02.png";
 } else if (randomCard == 3) {
   toJPG.style.backgroundImage = "url('../img/NEW SPACE CARD-03.png')";
   card.style.backgroundImage = "url('../img/NEW SPACE CARD-03.png')";
-  cardBack.src = "../img/card_back03.png"
+  cardBack.src = "../img/card_back03.png";
 } else if (randomCard == 4) {
   toJPG.style.backgroundImage = "url('../img/NEW SPACE CARD-04.png')";
   card.style.backgroundImage = "url('../img/NEW SPACE CARD-04.png')";
-  cardBack.src = "../img/card_back04.png"
+  cardBack.src = "../img/card_back04.png";
 } else if (randomCard == 5) {
   toJPG.style.backgroundImage = "url('../img/NEW SPACE CARD-05.png')";
   card.style.backgroundImage = "url('../img/NEW SPACE CARD-05.png')";
-  cardBack.src = "../img/card_back05.png"
+  cardBack.src = "../img/card_back05.png";
 }
+
+function updateTime() {
+  sessionStorage.setItem(
+    "musicTime",
+    document.getElementById("backgroundMusic").currentTime
+  );
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  let triangle1 = document.getElementById("triangle1");
+  let triangle2 = document.getElementById("triangle2");
+
+  spaceWordBtn.addEventListener("mouseenter", function () {
+    triangle1.src = "./img/triangle.gif";
+  });
+  spaceDesignBtn.addEventListener("mouseenter", function () {
+    triangle2.src = "./img/triangle.gif";
+  });
+
+  spaceWordBtn.addEventListener("mouseleave", function () {
+    triangle1.src = "./img/triangle.png";
+  });
+  spaceDesignBtn.addEventListener("mouseleave", function () {
+    triangle2.src = "./img/triangle.png";
+  });
+});
 
 function downloadJPG() {
   html2canvas(toJPG).then(function (canvas) {
@@ -61,18 +87,18 @@ function downloadJPG() {
     link.download = "card.jpg";
     link.click();
   });
-};
+}
 
 spaceWordBtn.addEventListener("mouseover", () => {
-  spaceWordBtn.src = "./img/space_word_btn_black_hover.png"
+  spaceWordBtn.src = "./img/space_word_btn_black_hover.png";
 });
 spaceWordBtn.addEventListener("mouseout", () => {
-  spaceWordBtn.src = "./img/space_word_btn_black.png"
+  spaceWordBtn.src = "./img/space_word_btn_black.png";
 });
 
 spaceDesignBtn.addEventListener("mouseover", () => {
-  spaceDesignBtn.src = "./img/space_design_btn_black_hover.png"
+  spaceDesignBtn.src = "./img/space_design_btn_black_hover.png";
 });
 spaceDesignBtn.addEventListener("mouseout", () => {
-  spaceDesignBtn.src = "./img/space_design_btn_black.png"
+  spaceDesignBtn.src = "./img/space_design_btn_black.png";
 });
